78f1b494645a8ec13f59a7778804dd09891f06b9
Daniel Choleva <daniel.choleva@betsys.com>
2025-12-01 09:36:35 +0100

    feat: refactor

--- src/app/pages/customer-filter/customer-filter.component.ts
+++ src/app/pages/customer-filter/customer-filter.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectorRef, Component, DestroyRef, inject } from '@angular/core';
 import { LayoutComponent } from '../../components/layout/layout.component';
 import { HttpClient } from '@angular/common/http';
-import { AbstractControl, FormArray, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { AsyncPipe } from '@angular/common';
 import { FilterDropdownComponent } from '../../components/filter-dropdown/filter-dropdown.component';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
@@ -15,6 +15,28 @@ interface Data {
         }[];
     }[];
 }
+
+type OptionFormGroup = FormGroup<{
+    option: FormControl<string>;
+    type: FormControl<string>;
+    value: FormControl<string>;
+}>;
+
+type PropertyFormGroup = FormGroup<{
+    property: FormControl<string>;
+    typeToDisplay: FormControl<string>;
+    optionToDisplay: FormControl<string>;
+    options: FormArray<OptionFormGroup>;
+}>;
+
+type EventFormGroup = FormGroup<{
+    type: FormControl<string>;
+    properties: FormArray<PropertyFormGroup>;
+}>;
+
+type CustomerFilterForm = FormGroup<{
+    events: FormArray<EventFormGroup>;
+}>;
 
 @Component({
     selector: 'app-customer-filter',
@@ -30,12 +52,12 @@ export class CustomerFilterComponent {
 
     data$ = this.http.get<Data>('https://br-fe-assignment.github.io/customer-events/events.json');
 
-    form = this.fb.group({
-        events: this.fb.nonNullable.array([]),
+    form: CustomerFilterForm = this.fb.group({
+        events: this.fb.nonNullable.array<EventFormGroup>([]),
     });
 
-    get events() {
-        return this.form.controls.events as FormArray;
+    get events(): FormArray<EventFormGroup> {
+        return this.form.controls.events;
     }
 
     ngOnInit() {
@@ -43,47 +65,34 @@ export class CustomerFilterComponent {
     }
 
     addEvent() {
-        const x = this.fb.group({
-            type: '',
-            properties: this.fb.nonNullable.array([]),
+        const event: EventFormGroup = this.fb.nonNullable.group({
+            type: this.fb.nonNullable.control(''),
+            properties: this.fb.nonNullable.array<PropertyFormGroup>([]),
         });
 
-        const control = <FormArray>this.form.get('events');
-        x.get('type')?.valueChanges
+        event.controls.type.valueChanges
             .pipe(
                 takeUntilDestroyed(this.destroyRef),
             )
-            .subscribe((val) => (<FormArray>x.get('properties'))?.clear());
+            .subscribe((val) => event.controls.properties.clear());
 
-        control.push(x);
+        this.events.push(event);
     }
 
     addProp(eventControl: AbstractControl) {
-        const x = this.fb.group({
-            property: '',
-            typeToDisplay: 'string',
-            optionToDisplay: '',
-            options: this.fb.nonNullable.array([]),
+        const prop: PropertyFormGroup = this.fb.nonNullable.group({
+            property: this.fb.nonNullable.control(''),
+            typeToDisplay: this.fb.nonNullable.control('string'),
+            optionToDisplay: this.fb.nonNullable.control(''),
+            options: this.fb.nonNullable.array<OptionFormGroup>([]),
         });
 
         const control = <FormArray>eventControl.get('properties');
-        control.push(x);
-    }
-
-    getProps(eventControl: AbstractControl) {
-        return eventControl.get('properties') as FormArray;
-    }
-
-    getOptions(propControl: AbstractControl) {
-        return propControl.get('options') as FormArray;
-    }
-
-    getPropOptions(eventControl: AbstractControl, data: Data) {
-        const x = eventControl.get('type')?.value;
-        return data.events[x]?.properties;
+        control.push(prop);
     }
 
     setOption(propControl: AbstractControl, event: any) {
+        console.log(propControl, event);
         const x = <FormArray>propControl.get('options');
         x.clear();
 
@@ -93,23 +102,23 @@ export class CustomerFilterComponent {
         });
 
         if (event.option.value === 'btw') {
-            const gtGroup = this.fb.group({
-                value: '',
-                option: 'gt',
-                type: 'number',
+            const gtGroup: OptionFormGroup = this.fb.nonNullable.group({
+                value: this.fb.nonNullable.control(''),
+                option: this.fb.nonNullable.control('gt'),
+                type: this.fb.nonNullable.control('number'),
             });
-            const ltGroup = this.fb.group({
-                value: '',
-                option: 'lt',
-                type: 'number',
+            const ltGroup: OptionFormGroup = this.fb.nonNullable.group({
+                value: this.fb.nonNullable.control(''),
+                option: this.fb.nonNullable.control('lt'),
+                type: this.fb.nonNullable.control('number'),
             });
             x.push(gtGroup);
             x.push(ltGroup);
         } else {
-            const y = this.fb.group({
-                option: event.option.value,
-                type: event.type,
-                value: '',
+            const y: OptionFormGroup = this.fb.nonNullable.group({
+                option: this.fb.nonNullable.control(event.option.value),
+                type: this.fb.nonNullable.control(event.type),
+                value: this.fb.nonNullable.control(''),
             });
 
             x.push(y);
@@ -125,20 +134,20 @@ export class CustomerFilterComponent {
 
     copyEvent(eventControlIndex: number) {
         const x = this.events.at(eventControlIndex);
-        const clone = this.fb.group({
-            type: x.get('type')?.value.toString(),
-            properties: this.fb.array(
-                x.get('properties')?.value.map((prop: any) =>
-                    this.fb.group({
-                        property: prop.property.toString(),
-                        typeToDisplay: prop.typeToDisplay.toString(),
-                        optionToDisplay: prop.optionToDisplay.toString(),
-                        options: this.fb.array(
-                            prop.options.map((option: any) =>
-                                this.fb.group({
-                                    option: option.option,
-                                    type: option.type,
-                                    value: option.value,
+        const clone: EventFormGroup = this.fb.nonNullable.group({
+            type: this.fb.nonNullable.control(x.controls.type.value),
+            properties: this.fb.nonNullable.array<PropertyFormGroup>(
+                (x.controls.properties.getRawValue() || []).map((prop) =>
+                    this.fb.nonNullable.group({
+                        property: this.fb.nonNullable.control(prop.property),
+                        typeToDisplay: this.fb.nonNullable.control(prop.typeToDisplay),
+                        optionToDisplay: this.fb.nonNullable.control(prop.optionToDisplay),
+                        options: this.fb.nonNullable.array<OptionFormGroup>(
+                            prop.options.map((option) =>
+                                this.fb.nonNullable.group({
+                                    option: this.fb.nonNullable.control(option.option),
+                                    type: this.fb.nonNullable.control(option.type),
+                                    value: this.fb.nonNullable.control(option.value),
                                 }),
                             ),
                         ),
@@ -147,13 +156,13 @@ export class CustomerFilterComponent {
             ),
         });
         
-        clone.get('type')?.valueChanges
+        clone.controls.type.valueChanges
             .pipe(takeUntilDestroyed(this.destroyRef))
             .subscribe((val) => {
-                (<FormArray>clone.get('properties'))?.clear();
+                clone.controls.properties.clear();
             });
         
-        this.events.push(clone, { emitEvent: true });
+        this.events.push(clone);
     }
 
     deleteProp(propControls: FormArray, index: number) {
@@ -166,7 +175,8 @@ export class CustomerFilterComponent {
         this.addEvent();
     }
 
-    applyFilters() {
+    applyFilters(data: Data) {
+        
         console.log(this.form.value);
     }
 }
